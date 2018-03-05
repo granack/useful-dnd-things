f6e49432fe807d57333e9c1ed63b09c28e226585
Granack <granack@gmail.com>
2018-03-05 06:58:24 -0800

    initial site formatting

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # useful-dnd-things
 eventually, some useful things for D&amp;D 5e
+
+- Bestiary - first view, then auto adjust (change size), then maybe, someday, full editor
+
+- Spell List - just what it sound slike. ya, there are a million of them out there already, so what?
+
+- Spell Book - save specific spells to custom lists (like a characater's spell book) for easy reference at the table
+
+- would be nice if it looks nice on a phone as well as desktop. but since it's running locally, that's a low priority.
+
+requires a web server: if yo python, run python -m SimpleHTTPServer in the repo's root directory
--- script/main.js
+++ script/main.js
@@ -1,1 +1,42 @@
-// nothing here
+// script store
+
+function fetchStatus(response) {
+  if (response.status >= 200 && response.status < 300) {
+    return Promise.resolve(response)
+  } else {
+    return Promise.reject(new Error(response.statusText))
+  }
+}
+
+function fetchText(response) {
+  return response.text()
+}
+
+function loadData(data){
+  // alert(data); // works
+}
+
+document.addEventListener("DOMContentLoaded", function(){
+    // function can go here, runs after DOM is parsed before images and css are downlaoded
+    // in other words, use this place to load menus or whatever
+
+  fetch('../data/gbestiary.xml')
+  .then(fetchStatus)
+  .then(fetchText)
+  .then(loadData)
+  .catch(function(err) {
+    console.log('Fetch Error :-S', err);
+  });
+
+});
+
+var menuItems = document.getElementsByClassName("side-menu-item");
+for (let item of menuItems){
+  item.addEventListener("click", function(){
+    for (let off of document.getElementsByClassName("side-active")){
+      off.classList.remove("side-active");
+    }
+    this.classList.add("side-active");
+    // placeholder for function to load appropriate data
+  });
+}
