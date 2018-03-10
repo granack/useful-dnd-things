a0ef451e315eb75b73fd9e66ce82a8393f030db9
Granack <granack@gmail.com>
2018-03-09 21:18:37 -0800

    module detection, loads module menu

--- script/main.js
+++ script/main.js
@@ -1,6 +1,21 @@
 // script store
 
 var xmlParser= new DOMParser();
+var moduleList = new Array(0);
+
+function getTextFile(textfile, callback){
+    fetch(textfile)
+    .then(fetchStatus)
+    .then(fetchText)
+    .then(callback)
+    .catch(function(err) {
+      console.log('Fetch Error :-S', err);
+  });
+}
+
+function getModuleList(){
+
+}
 
 function fetchStatus(response) {
   if (response.status >= 200 && response.status < 300) {
@@ -14,50 +29,59 @@ function fetchText(response) {
   return response.text()
 }
 
-function loadModules(data){
-  var modulesXML = xmlParser.parseFromString(data, "text/xml");
-  console.log(modulesXML);
-  var moduleList=modulesXML.getElementsByTagName("module");
-  console.log("length:",moduleList.length);
-  var modules = [];
-  for (let i=0;i<moduleList.length;i++){
-    let name=moduleList[i].getElementsByTagName("name")[0].childNodes[0].nodeValue;
-    let desc=moduleList[i].getElementsByTagName("description")[0].childNodes[0].nodeValue;
-    let settings=moduleList[i].getElementsByTagName("settings-file")[0].childNodes[0].nodeValue;
-    let module ={name: name, description: desc, settings:settings};
-    console.log("module.name:",module.name);
-    console.log("module.desc:",module.description);
-    console.log("module.settings:",module.settings);
-    modules.push(module);
-  }
-  console.log(modules[1].name);
-  var navMenu = document.getElementsByClassName('nav-menu');
-  let menuItemTemplate=document.createElement("DIV");
-  menuItemTemplate.classList.add("nav-menu-item");
-  menuItemTemplate.appendChild(document.createElement("P"));
-  navMenu[0].innerHTML=""
-  for (let mod of modules){
-    let node = menuItemTemplate.cloneNode(true);
-    node.children[0].appendChild(document.createTextNode(mod.name));
-    navMenu[0].appendChild(node);
-  }
+function loadModules(){
+      getTextFile("/modules/", function(text){
+        let result = new DOMParser().parseFromString(text, "text/html");
+        mods = result.getElementsByTagName("li");
+        for (let i=0;i<mods.length;i++) {
+          let mod = {};
+          mod.name = mods[i].innerText.split('/', 1)[0];
+          mod.number = i;
+          mod.path = "/modules/" + mod.name + "/" + mod.name + ".js";
+          moduleList.push(mod);
+
+        }
+        var modMenu = document.getElementById('mod-menu');
+        let menuItemTemplate=document.createElement("DIV");
+        menuItemTemplate.classList.add("mod-menu-item");
+        menuItemTemplate.appendChild(document.createElement("P"));
+        modMenu.innerHTML=""
+        for (let mod of moduleList){
+          let node = menuItemTemplate.cloneNode(true);
+          node.addEventListener("click", function(){
+            if(!this.classList.contains("mod-active")){
+              for (let off of document.getElementsByClassName("mod-active")){
+                off.classList.remove("mod-active");
+              }
+              this.classList.add("mod-active");
+              loadMod(this.getAttribute("number"));
+            }
+            // TODO: load the module... maybe unload previous somehow.
+          });
+          node.children[0].appendChild(document.createTextNode(mod.name));
+          node.setAttribute("number", mod.number);
+          modMenu.appendChild(node);
+        }
+    });
+
+
+}
+
+function loadMod(modnum){
+  document.getElementById("content").innerText = modnum;
 }
 
 function loadData(data){
   // alert(data); // works
 }
 
-document.addEventListener("DOMContentLoaded", function(){
-    // function can go here, runs after DOM is parsed before images and css are downlaoded
+document.addEventListener("DOMContentLoaded",  function(){
+    // functions can go here, runs after DOM is parsed before images and css are downlaoded
     // in other words, use this place to load menus or whatever
 
-  fetch('../settings/modules.xml')
-  .then(fetchStatus)
-  .then(fetchText)
-  .then(loadModules)
-  .catch(function(err) {
-    console.log('Fetch Error :-S', err);
-  });
+    loadModules();
+
+
 
 });
 
