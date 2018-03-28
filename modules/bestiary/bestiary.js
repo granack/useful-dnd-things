82b9c6c609bfc2efdc2f7974844b457e6a2efeff
Granack <granack@gmail.com>
2018-03-27 22:12:32 -0700

    fix button click

--- modules/bestiary/bestiary.js
+++ modules/bestiary/bestiary.js
@@ -23,11 +23,7 @@ newstyle.setAttribute("type", "text/css");
 newstyle.setAttribute("href", "https://fonts.googleapis.com/css?family=Noto+Sans:400,700,400italic,700italic");
 document.getElementsByTagName("head")[0].appendChild(newstyle);
 
-
-
 getTextFile("/modules/bestiary/bestiary.html",processBestiaryHtml);
-
-
 
 function processBestiaryHtml(myFile) {
   let htmlDoc = xmlParser.parseFromString(myFile, "text/html");
@@ -45,23 +41,9 @@ export function load(content){
   bContent=content;
   bContent.innerText="Bestiary loaded.";
   loadMenu(bMenuItemList);
-
-
 }
 
 export function menuClick(index) {
-  // alert("Menu item" + index + "clicked!.");
-  /*let xsltProcessor = new XSLTProcessor();
-  xsltProcessor.importStylesheet(bxsl);
-  let resultDocument = xsltProcessor.transformToFragment(bMonsters[index], document);
-  if(resultDocument==null){
-    bContent.innerText="result Doc null: transform completed but result was invalid HTML";
-  } else if(resultDocument.childElementCount==0) {
-    bContent.innerText = bxsl.childNodes[1].textContent;
-  } else {
-    bContent.innerText = "";
-    bContent.appendChild(resultDocument);
-  } */
   bContent.innerText = "";
-  bContent.appendChild(bMonsters[index]);
+  bContent.appendChild(bMonsters[index].cloneNode(true));
 }
